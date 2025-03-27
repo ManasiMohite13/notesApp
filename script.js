983c7e359cc3d199fd7e7ad9e59c74a212beb57d
ManasiMohite13 <manasimohite3553@gmail.com>
2025-03-27 16:49:42 +0530

    Add dynamic note creation with delete icon

---
+++ script.js
@@ -0,0 +1,19 @@
+const createbtn = document.querySelector('.btn');
+const notesContainer = document.querySelector('.notes-container');
+let notes = document.querySelectorAll('.input-box');
+
+createbtn.addEventListener('click', function () {
+    let inputBox = document.createElement("p");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contentEditable", "true");
+    let img = document.createElement("img");
+    img.src = "assets/delete.png";
+
+    inputBox.appendChild(img);
+    notesContainer.appendChild(inputBox);
+})
+
+    .
+
+
+
