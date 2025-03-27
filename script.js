921529e0bfffc1f4c6f7a10f2a06b63204116800
ManasiMohite13 <manasimohite3553@gmail.com>
2025-03-27 17:27:51 +0530

    Add localStorage and event listeners for notes persistence

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const createbtn = document.querySelector('.btn');
 const notesContainer = document.querySelector('.notes-container');
 let notes = document.querySelectorAll('.input-box');
 
+showNotes();
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createbtn.addEventListener('click', function () {
     let inputBox = document.createElement("p");
     inputBox.className = "input-box";
@@ -11,13 +19,32 @@ createbtn.addEventListener('click', function () {
 
     inputBox.appendChild(img);
     notesContainer.appendChild(inputBox);
+    updateStorage(); 
+});
+
+// Remove note on clicking the delete icon
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        })
+    }
+});
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
 })
 
-    notesContainer.addEventListener("click",function(e){
-        if(e.target.tagName === "IMG"){
-            e.target.parentElement.remove();
-        }
-    })
 
 
 
